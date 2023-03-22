f4294c5be1682c4380a46093a6c2aee0560db5fd
bp0002 <111483528+baip0002@users.noreply.github.com>
2023-03-22 15:49:09 +0800

    # fix: Add SimpleInOut

--- crates/render/src/lib.rs
+++ crates/render/src/lib.rs
@@ -37,6 +37,8 @@ mod system;
 
 /// 渲染图
 pub use graph::*;
+use pi_hash::{XHashSet, XHashMap};
+use pi_render::{components::view::target_alloc::ShareTargetView, depend_graph::param::{InParam, OutParam}};
 /// 渲染 插件
 pub use plugin::*;
 /// 单例
@@ -44,3 +46,31 @@ pub use resource::*;
 
 /// 标签
 pub use clear_node::CLEAR_WIDNOW_NODE;
+
+#[derive(Default, Clone)]
+pub struct SimpleInOut {
+    target: Option<ShareTargetView>,
+}
+impl InParam for SimpleInOut {
+    fn can_fill<O: pi_render::depend_graph::param::OutParam + ?Sized>(
+        &self,
+        map: &mut XHashMap<std::any::TypeId, Vec<NodeId>>,
+        pre_id: NodeId,
+        out_param: &O,
+    ) -> bool {
+        true
+    }
+
+    fn fill_from<O: pi_render::depend_graph::param::OutParam + ?Sized>(&mut self, pre_id: NodeId, out_param: &O) -> bool {
+        true
+    }
+}
+impl OutParam for SimpleInOut {
+    fn can_fill(&self, set: &mut Option<&mut XHashSet<std::any::TypeId>>, ty: std::any::TypeId) -> bool {
+        true
+    }
+
+    fn fill_to(&self, this_id: NodeId, to: &mut dyn pi_render::depend_graph::param::Assign, ty: std::any::TypeId) -> bool {
+        true
+    }
+}
