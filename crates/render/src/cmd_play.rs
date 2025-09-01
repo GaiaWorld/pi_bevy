58a3e1ab16fa64be0ba07040b8bc7edb3ef008f4
bp0002 <bp950722@outlook.com>
2025-09-01 11:57:27 +0800

    # fix: cmd play

--- crates/render/src/cmd_play.rs
+++ crates/render/src/cmd_play.rs
@@ -1,9 +1,11 @@
 
 use bevy_window::FrameState;
 use pi_hash::XHashMap;
+use pi_world::insert::Insert;
 use pi_world::prelude::IntoSystemConfigs;
 use pi_world::schedule_config::IntoSystemSetConfigs;
 use pi_world::single_res::{SingleRes, SingleResMut};
+use pi_world::system_params::SystemParam;
 use pi_world::{prelude::Plugin, schedule::First, schedule_config::SystemSet, world::{Entity, World}};
 use serde::{Deserialize, Serialize};
 
@@ -136,6 +138,8 @@ impl Records {
     pub fn record_create(&mut self, entity: Entity) {
         if let Some(list) = self.list.last_mut() {
             list.entities.push(entity);
+        } else {
+            log::error!(">>> list ilen is 0");
         }
     }
     pub fn record(&mut self, key: KeyRecord, serialized: Vec<u8>) {
@@ -145,6 +149,8 @@ impl Records {
     }
 
     pub fn replay(world: &mut World) {
+        log::error!(">>> replay");
+
         let records = world.get_single_res::<Records>().unwrap();
         let records_list_len = records.list.len();
         let play_state = world.get_single_res_mut::<PlayState>().unwrap();
@@ -167,31 +173,41 @@ impl Records {
             return;
         }
 
-        // 慢速播放设置
-        if play_state.await_frame_count > 0 {
-            play_state.await_frame_count -= 1;
-            play_state.playresult = EReplayResult::AwaitFrame;
-            return;
-        } else {
-            if play_state.speed < 1.0 && play_state.speed > 0.0 {
-                play_state.await_frame_count = (1.0 / play_state.speed) as usize;
-            }
-        }
+        // // 慢速播放设置
+        // if play_state.await_frame_count > 0 {
+        //     play_state.await_frame_count -= 1;
+        //     play_state.playresult = EReplayResult::AwaitFrame;
+        //     return;
+        // } else {
+        //     if play_state.speed < 1.0 && play_state.speed > 0.0 {
+        //         play_state.await_frame_count = (1.0 / play_state.speed) as usize;
+        //     }
+        // }
 
         play_state.cur_frame_count += 1;
 
         let cur_frame_count = play_state.cur_frame_count;
-        let next_state_index = play_state.next_state_index;
+        let mut next_state_index = play_state.next_state_index;
+    loop {
         let records = world.get_single_res::<Records>().unwrap();
-
         let r = &records.list[next_state_index];
+        let frame_index = r.frame_index as f32 ;
+        let play_state = world.get_single_res_mut::<PlayState>().unwrap();
+
         // 还需要继续播放一些空帧
-        if r.frame_index < cur_frame_count {
-            let play_state = world.get_single_res_mut::<PlayState>().unwrap();
+        if frame_index as f32 > play_state.cur_frame_count  as f32 * play_state.speed {
             play_state.playresult = EReplayResult::NullFrame;
-            return;
+            break;
         }
 
+        // if r.frame_index < cur_frame_count {
+        //     let play_state = world.get_single_res_mut::<PlayState>().unwrap();
+        //     play_state.playresult = EReplayResult::NullFrame;
+        //     return;
+        // }
+
+        let records = world.get_single_res::<Records>().unwrap();
+        let r = &records.list[next_state_index];
         // 创建Entity, 建立映射
         let node_map = {
             let createlist = r.entities.clone();
@@ -201,10 +217,13 @@ impl Records {
                 newentities.push(world.spawn_empty());
             }
 
+            log::error!("Commands Count: {:?}", (r.cmds.len(), r.frame_index));
+            log::error!("Entitiy Count: {:?}", r.entities);
             let play_state = world.get_single_res_mut::<PlayState>().unwrap();
-            let mut node_map = play_state.node_map.clone();
+            let node_map = &mut play_state.node_map;
             for i in 0..newlen {
                 if node_map.contains_key(&createlist[i]) == false {
+                    log::error!("{:?}", (createlist[i], newentities[i]));
                     node_map.insert(createlist[i], newentities[i]);
                 }
             }
@@ -212,13 +231,8 @@ impl Records {
             node_map.clone()
         };
 
-        let play_state = world.get_single_res_mut::<PlayState>().unwrap();
-        let next_state_index = play_state.next_state_index;
-        play_state.next_state_index += 1;
-
         let records = world.get_single_res::<Records>().unwrap();
         let r = &records.list[next_state_index];
-
         let palycalls = records.palycalls.clone();
         r.cmds.clone().iter().for_each(|item| {
             if let Some(playcall) = palycalls.get(&item.0) {
@@ -227,7 +241,10 @@ impl Records {
         });
 
         let play_state = world.get_single_res_mut::<PlayState>().unwrap();
+        play_state.next_state_index += 1;
         play_state.playresult = EReplayResult::Ok;
+        next_state_index = play_state.next_state_index;
+    }
     }
 }
 
@@ -252,6 +269,11 @@ pub fn sys_cmd_replay(world: &mut World) {
     Records::replay(world);
 }
 
+pub fn sys_frame_count(
+    mut records: SingleResMut<Records>,
+) {
+    records.cur_frame_count += 1; // 记录帧数量
+}
 pub struct GlobalCmdTracePlugin {
     pub option: TraceOption,
 }
@@ -265,6 +287,9 @@ impl Plugin for GlobalCmdTracePlugin {
             TraceOption::Record => {
                 app.add_system(First, sys_cmd_record
                     .in_set(StageCMDTrace::Trace)
+                );
+                app.add_system(First, sys_frame_count
+                    .in_set(StageCMDTrace::After)
                 );
             }
             TraceOption::Play => {
